b6dae27e9c8191e96102d94e22667a446ccdec0f
Johan Burmester <johan.burmester@hemnet.se>
2020-11-02 17:22:43 +0100

    first commit

---
+++ example.js
@@ -0,0 +1,37 @@
+const { exception } = require('console');
+const puppeteer = require('puppeteer');
+
+(async (event, context) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    try{
+        const str = "https://www.hemnet.se/saljkollen/17078586/7a0ea7ad148accd3c5cbfbe91af48c4548da1e9e6cdf74a9cbd463226e87579c?asdfasdfas"
+        const url = str.split("?")[0]+'?role=admin';
+        await page.goto(url)
+        await page.setViewport({
+            width: 1200,
+            height: 800
+        });
+        await page.waitForTimeout(500)
+        //const visits = await page.$eval('.sellers-dashboard__primary-number', el => el.innerText)
+        const raketen = await page.$eval('.sellers-dashboard__secondary-number', el => el.innerText)
+        return {
+            statusCode: 200,
+            body: "Raketen: " + raketen
+          }
+    } catch(err) {
+        try{
+            const visits = await page.$eval('.seller-listing__times-viewed-counter', el => el.innerText)
+            return {
+                statusCode: 200,
+                body: "Visits: " + visits
+              }
+        }catch (err2){
+            console.log("error",err, err2)
+            return {
+                statusCode: 500
+              }
+        }
+    }
+  await browser.close()
+})()
